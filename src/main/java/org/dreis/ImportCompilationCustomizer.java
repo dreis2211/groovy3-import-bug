7c25839d74d6d24a47c2d7c2168149dafbd4e88b
Christoph Dreis <christoph.dreis@freenet.de>
2020-02-28 20:09:25 +0100

    Show workaround directly in comment

--- src/main/java/org/dreis/ImportCompilationCustomizer.java
+++ src/main/java/org/dreis/ImportCompilationCustomizer.java
@@ -9,6 +9,7 @@ import org.codehaus.groovy.control.customizers.CompilationCustomizer;
 import org.codehaus.groovy.control.customizers.ImportCustomizer;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class ImportCompilationCustomizer extends CompilationCustomizer {
 
@@ -21,7 +22,9 @@ public class ImportCompilationCustomizer extends CompilationCustomizer {
             throws CompilationFailedException {
 
         ImportCustomizer importCustomizer = new ImportCustomizer();
-        for (CompilerAutoConfiguration autoConfiguration : Arrays.asList(new ApplicationCompilerAutoConfiguration(), new ExampleCompilerAutoConfiguration())) {
+        List<CompilerAutoConfiguration> compilerAutoConfigurations = Arrays.asList(new ApplicationCompilerAutoConfiguration(), new ExampleCompilerAutoConfiguration());
+        for (CompilerAutoConfiguration autoConfiguration : compilerAutoConfigurations) {
+            // if (source.getAST().getClasses().stream().anyMatch(autoConfiguration::matches)) {
             if (autoConfiguration.matches(classNode)) {
                 autoConfiguration.applyImports(importCustomizer);
                 importCustomizer.call(source, context, classNode);
